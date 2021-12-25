ec85864e8939dc100e1ef982ba2bf5d156a3e332
Darkpppet <darkpppet@cau.ac.kr>
2021-12-25 11:41:25 +0000

    2021.12.25

--- DiamondV/4149.cs
+++ DiamondV/4149.cs
@@ -1,18 +1,44 @@
 using System;
 using System.Text;
+using System.Collections.Generic;
 
-StringBuilder outputBuilder = new();
-
-long n = long.Parse(Console.ReadLine());
-
-for (long i = 2; n > 1; i++)
+static class Prime
 {
-	if (n % i == 0)
-	{
-		n /= i;
-		outputBuilder.AppendLine(i.ToString());
-		i--;
-	}
+    public static long CalcGcd(long a, long b)
+    {
+    }
+    
+    public static bool isPrime(long n)
+    {
+        
+        
+    }
+    
+    private long F(long x, long n) => (x * x) % n;
+    
+    public static List<long> Factorize(long n)
+    {
+        
+        
+    }
+    
 }
 
-Console.Write(outputBuilder.ToString());
+class Program
+{
+    static void Main(string[] args)
+    {
+        long n = long.Parse(Console.ReadLine());
+        
+        List<long> primes = Prime.Factorize(n);
+        
+        primes.Sort();
+        
+        StringBuilder outputBuilder = new();
+        
+        foreach (long prime in primes)
+            outputBuilder.AppendLine(prime.ToString());
+        
+        Console.WriteLine(outputBuilder.ToString());
+    }
+}
