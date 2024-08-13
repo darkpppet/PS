8c58bc6a2c30dc47038e21bb818f198d72185baf
darkpppet <ruldaiz@naver.com>
2024-08-14 01:50:38 +0900

    2024-08-14

--- Baekjoon/SilverI/1389.cs
+++ Baekjoon/SilverI/1389.cs
@@ -1,56 +1,65 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
 string[] input = Console.ReadLine().Split(' ');
 
 int n = int.Parse(input[0]);
 int m = int.Parse(input[1]);
 
-int[,] relationships = new int[n, n];
+int[,] relationships = new int[n + 1, n + 1];
+
+for (int i = 1; i <= n; i++)
+{
+    for (int j = 1; j <= n; j++)
+    {
+        relationships[i, j] = int.MaxValue / 2;
+    }
+}
 
 for (int i = 0; i < m; i++)
 {
-	input = Console.ReadLine().Split(' ');
+    input = Console.ReadLine().Split(' ');
 	
-	int a = int.Parse(input[0]);
-	int b = int.Parse(input[1]);
+    int a = int.Parse(input[0]);
+    int b = int.Parse(input[1]);
 	
-	relationships[a - 1, b - 1] = 1;
-	relationships[b - 1, a - 1] = 1;
+    relationships[a, b] = 1;
+    relationships[b, a] = 1;
 }
 
-int[] sums = new int[n];
-
-for (int i = 0; i < n; i++)
+for (int mid = 1; mid <= n; mid++)
 {
-	List<int> before = new() { i };
-	int[] checkedPerson = new int[n];
-	
-	int count = 0;
-	int level = 0;
-	
-	while (before.Count > 0)
-	{
-		List<int> tempList = new();
-		
-		foreach (int person in before)
-		{
-			checkedPerson[person] = 1;
-			count += level;
-			
-			for (int j = 0; j < n; j++)
-			{
-				if (relationships[person, j] == 1 && checkedPerson[j] != 1)
-					tempList.Add(j);
-			}
-		}
-		
-		before = new(tempList);
-		level++;
-	}
-	
-	sums[i] = count;
+    for (int start = 1; start <= n; start++)
+    {
+        for (int end = 1; end <= n; end++)
+        {
+            if (relationships[start, end] > relationships[start, mid] + relationships[mid, end])
+            {
+                relationships[start, end] = relationships[start, mid] + relationships[mid, end];
+            }
+        }
+    }
 }
 
-Console.WriteLine(Array.IndexOf(sums, sums.Min()) + 1);
+int[] nums = new int[n + 1];
+
+int min = int.MaxValue;
+int minIndex = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    nums[i] = 0;
+    for (int j = 1; j <= n; j++)
+    {
+        if (i != j)
+        {
+            nums[i] += relationships[i, j];
+        }
+    }
+    
+    if (nums[i] < min)
+    {
+        min = nums[i];
+        minIndex = i;
+    }
+    
+}
+
+Console.WriteLine(minIndex);
