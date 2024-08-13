24e62228b28a6f880882fbd38d324b96c99ac7b4
darkpppet <ruldaiz@naver.com>
2024-08-14 01:57:10 +0900

    2024-08-14

--- Baekjoon/BronzeIV/31403.cs
+++ Baekjoon/BronzeIV/31403.cs
@@ -1,57 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿string a = Console.ReadLine();
+string b = Console.ReadLine();
+string c = Console.ReadLine();
 
-string[] input = Console.ReadLine().Split(' ');
-
-int n = int.Parse(input[0]);
-int m = int.Parse(input[1]);
-
-int[,] relationships = new int[n, n];
-
-for (int i = 0; i < m; i++)
-{
-	input = Console.ReadLine().Split(' ');
-	
-	int a = int.Parse(input[0]);
-	int b = int.Parse(input[1]);
-	
-	relationships[a - 1, b - 1] = 1;
-	relationships[b - 1, a - 1] = 1;
-}
-
-int[] sums = new int[n];
-
-for (int i = 0; i < n; i++)
-{
-	List<int> before = new() { i };
-	int[] checkedPerson = new int[n];
-	
-	int count = 0;
-	int level = 0;
-	
-	List<int> tempList;
-	while (before.Count > 0)
-	{ 
-		tempList = new();
-		
-		foreach (int person in before)
-		{
-			checkedPerson[person] = 1;
-			count += level;
-			
-			for (int j = 0; j < n; j++)
-			{
-				if (relationships[person, j] == 1 && checkedPerson[j] != 1)
-					tempList.Add(j);
-			}
-		}
-		
-		before = new(tempList);
-		level++;
-	}
-	
-	sums[i] = count;
-}
-
-Console.WriteLine(Array.IndexOf(sums, sums.Min()) + 1);
+Console.WriteLine(int.Parse(a) + int.Parse(b) - int.Parse(c));
+Console.WriteLine(int.Parse(a + b) - int.Parse(c));
