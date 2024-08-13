89d2ddb818a7b9d0964062a71629b32e57bae5b7
darkpppet <ruldaiz@naver.com>
2024-08-14 01:17:26 +0900

    2024-08-14

---
+++ Baekjoon/DiamondII/14853.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+int t = int.Parse(Console.ReadLine());
+
+StringBuilder outputBuilder = new();
+
+for (int tc = 0; tc < t; tc++)
+{
+    string[] input = Console.ReadLine().Split(' ');
+
+    double n1 = double.Parse(input[0]);
+    double m1 = double.Parse(input[1]);
+    double n2 = double.Parse(input[2]);
+    double m2 = double.Parse(input[3]);
+
+    double temp = 1;
+
+    for (int i = 0; i <= n2; i++)
+    {
+        temp *= (n1 - m1 + 1 + i) / (n1 + 2 + i);
+    }
+
+    double answer = temp;
+
+    for (int i = 1; i <= m2; i++)
+    {
+        temp *= (m1 + i) * (n2 + 2 - i) / i  / (n1 + n2 - m1 + 2 - i);
+        answer += temp;
+    }
+
+    outputBuilder.AppendLine(answer.ToString());
+}
+
+Console.Write(outputBuilder.ToString());
